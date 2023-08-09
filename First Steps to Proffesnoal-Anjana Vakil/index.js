b47ba3c26a0bcd5c71c6aef35a90961f18977618
himanshu-singh <intern3@vama.app>
2023-08-09 11:13:07 +0530

    new

---
+++ First Steps to Proffesnoal-Anjana Vakil/index.js
@@ -0,0 +1,39 @@
+// what is js?
+// makes web interative 
+
+let js = {
+    name:'javascript',
+    abbreviation:"js",
+    isAwesome:true,
+    officialSpec:"ECMAScript",
+    birthYear:"1995",
+    creater:"Brendan Eich"
+};
+
+if (js.isAwesome){
+    console.log("hell yes js!");
+}
+
+/* core web technologies */
+// html - noun
+// css - adjective
+// javascript - verb
+
+/* where to write js 
+1.The brwoser js console
+2.local text editor
+3.online editor
+*/
+
+/* 
+document - built in object which represents whole html document
+document.title - title of the document
+document.body - body of the document
+document.head - head of the document
+document.body.children - children in the body of the document
+document.getElementsById - get elements based on id attribute
+document.querySelector("#board") - in case of id attribute
+document.getElementsByTagName("h1")
+document.querySelectorAll("h1")
+document.querySelectorAll(".player") - in case of class attribute
+*/
